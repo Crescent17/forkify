e5a98c001042db458f2f68a8dcd24228ea982daf
Marat <94042173+Crescent17@users.noreply.github.com>
2023-08-17 17:42:43 +0300

    add renderError and renderMessage functions to display messages in UI

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -3,6 +3,8 @@ import icons from '../../img/icons.svg';
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
+  #errorMessage = 'We could not find that recipe. Please try another one!';
+  #message = 'Success';
 
   render(data) {
     this.#data = data;
@@ -21,6 +23,34 @@ class RecipeView {
       <svg>
         <use href='${icons}#icon-loader'></use>
       </svg>
+    </div>`;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderError(message = this.#errorMessage) {
+    const markup = `
+    <div class='error'>
+      <div>
+        <svg>
+          <use href='${icons}#icon-alert-triangle'</use>
+        </svg>
+      </div>
+      <p>${message}</p>
+    </div>`;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderMessage(message = this.#message) {
+    const markup = `
+    <div class='message'>
+      <div>
+        <svg>
+          <use href='${icons}#icon-smile'</use>
+        </svg>
+      </div>
+      <p>${message}</p>
     </div>`;
     this.#clear();
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
