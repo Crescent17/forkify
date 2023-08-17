2ec2f603ba79cfd2038d2e31e454d808e9de3591
Marat <94042173+Crescent17@users.noreply.github.com>
2023-08-17 18:31:53 +0300

    create controlSearchResults method to link model with view and call addHandlerSearch method in init function

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,6 @@
 import * as model from './model';
 import recipeView from './views/recipeView';
+import searchView from './views/searchView';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -16,8 +17,20 @@ async function controlRecipes() {
   }
 }
 
+async function controlSearchResults() {
+  try {
+    const query = searchView.getQuery();
+    if (!query) return;
+    await model.loadSearchResults(query);
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 function init() {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 }
 
 init();
