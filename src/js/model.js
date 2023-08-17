bfa58b61044b0e4c4995665d3088885728dbd508
Marat <94042173+Crescent17@users.noreply.github.com>
2023-08-17 18:29:56 +0300

    add logic to search recipes

--- src/js/model.js
+++ src/js/model.js
@@ -2,12 +2,16 @@ import { API_URL } from './config';
 import { getJSON } from './helpers';
 
 export const state = {
-  recipe: {}
+  recipe: {},
+  search: {
+    query: '',
+    results: []
+  }
 };
 
 export async function loadRecipe(id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
     const { recipe } = data.data;
     state.recipe = {
       id: recipe.id,
@@ -19,6 +23,23 @@ export async function loadRecipe(id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients
     };
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function loadSearchResults(query) {
+  try {
+    state.search.query = query;
+    const { data } = await getJSON(`${API_URL}?search=${query}`);
+    state.search.results = data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url
+      };
+    });
   } catch (err) {
     throw err;
   }
