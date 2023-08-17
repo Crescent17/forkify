87da26255265ff75a32b3609047808f93d643c8d
Marat <94042173+Crescent17@users.noreply.github.com>
2023-08-17 12:07:29 +0300

    create render spinner function and call it on recipe load

--- src/js/controller.js
+++ src/js/controller.js
@@ -12,7 +12,19 @@ const timeout = function(s) {
   });
 };
 
+function renderSpinner(parentEl) {
+  const markup = `
+      <div class='spinner'>
+      <svg>
+        <use href='${icons}#icon-loader'></use>
+      </svg>
+    </div>`;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+}
+
 async function showRecipe() {
+  renderSpinner(recipeContainer);
   try {
     const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
     const data = await res.json();
