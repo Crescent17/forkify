f0a4f01176a9d71d319a7f96991a546aa74f54e4
Marat <94042173+Crescent17@users.noreply.github.com>
2023-08-18 16:47:52 +0300

    remove unnecessary check and logs

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -12,14 +12,11 @@ export default class View {
   }
 
   update(data) {
-    if (!data || (Array.isArray(data) && data.length === 0)) return this.renderError();
     this._data = data;
     const newMarkup = this._generateMarkup();
     const newDOM = document.createRange().createContextualFragment(newMarkup);
     const newElements = Array.from(newDOM.querySelectorAll('*'));
     const curElements = Array.from(this._parentElement.querySelectorAll('*'));
-    console.log(curElements);
-    console.log(newElements);
     newElements.forEach((newEl, i) => {
       const curEl = curElements[i];
       if (!curEl.isEqualNode(newEl) && newEl?.firstChild.nodeValue.trim() !== '') {
