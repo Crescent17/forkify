80876cc19398dbe1c7bcc54572465cf8ca060814
Marat <94042173+Crescent17@users.noreply.github.com>
2023-08-18 13:17:50 +0300

    render page after clicking pagination buttons

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,6 +5,7 @@ import resultsView from './views/resultsView';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import paginationView from './views/paginationView';
 
 async function controlRecipes() {
   try {
@@ -24,15 +25,22 @@ async function controlSearchResults() {
     const query = searchView.getQuery();
     if (!query) return;
     await model.loadSearchResults(query);
-    resultsView.render(model.state.search.results)
+    resultsView.render(model.getSearchResultsPage(model.state.search.page));
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 }
 
+function controlPagination(goToPage) {
+  resultsView.render(model.getSearchResultsPage(goToPage));
+  paginationView.render(model.state.search);
+}
+
 function init() {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 }
 
 init();
