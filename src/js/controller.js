1210e6adbb5fab8f55aec66ad89f0a8243911366
Marat <94042173+Crescent17@users.noreply.github.com>
2023-08-21 12:40:29 +0300

    update search results to load only that element that was changed

--- src/js/controller.js
+++ src/js/controller.js
@@ -12,6 +12,7 @@ async function controlRecipes() {
     const id = window.location.hash.slice(1);
     if (!id) return;
     recipeView.renderSpinner();
+    resultsView.update(model.getSearchResultsPage());
     await model.loadRecipe(id);
     recipeView.render(model.state.recipe);
   } catch (err) {
@@ -46,7 +47,7 @@ function init() {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
-  recipeView.addHandlerUpdateServings(controlServings)
+  recipeView.addHandlerUpdateServings(controlServings);
 }
 
 init();
