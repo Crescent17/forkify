d8beec063ba5e53b0ccf524f0ecd62dbb21284c1
Marat <94042173+Crescent17@users.noreply.github.com>
2023-08-22 12:26:43 +0300

    save bookmarks into local storage

--- src/js/model.js
+++ src/js/model.js
@@ -63,13 +63,25 @@ export function updateServings(newServings) {
   state.recipe.servings = newServings;
 }
 
+function persistBookmarks() {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+}
+
 export function addBookmark(recipe) {
   state.bookmarks.push(recipe);
   if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+  persistBookmarks();
 }
 
 export function deleteBookmark(id) {
   const index = state.bookmarks.findIndex(el => el.id === id);
   state.bookmarks.splice(index, 1);
   if (id === state.recipe.id) state.recipe.bookmarked = false;
+  persistBookmarks();
 }
+
+function init() {
+  const storage = localStorage.getItem('bookmarks');
+  if (storage) state.bookmarks = JSON.parse(storage);
+}
+init();
