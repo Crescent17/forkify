462999a456916901fc81452e96b80a0acf72bd62
Marat <94042173+Crescent17@users.noreply.github.com>
2023-08-22 14:05:46 +0300

    add new variables in config file

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '3fd86252-656e-4d44-a1a7-d304e3090254'
+export const MODAL_CLOSE_SEC = 2.5
