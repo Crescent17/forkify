55a35f744b542f655d921e4fc37b6d10063fbd2d
Marat <94042173+Crescent17@users.noreply.github.com>
2023-08-22 14:50:43 +0300

    use new AJAX function, in upload recipe function instead of replacing white spaces in ingredients just trim them, save key (if it exists) into search results objects

--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,5 @@
 import { API_URL, KEY, RES_PER_PAGE } from './config';
-import { getJSON, sendJSON } from './helpers';
+import { AJAX } from './helpers';
 
 export const state = {
   recipe: {},
@@ -29,7 +29,7 @@ function createRecipeObject(data) {
 
 export async function loadRecipe(id) {
   try {
-    const data = await getJSON(`${API_URL}${id}`);
+    const data = await AJAX(`${API_URL}${id}?key=${KEY}`);
     state.recipe = createRecipeObject(data);
     if (state.bookmarks.some(bookmarked => bookmarked.id === id)) state.recipe.bookmarked = true;
     else state.recipe.bookmarked = false;
@@ -41,13 +41,14 @@ export async function loadRecipe(id) {
 export async function loadSearchResults(query) {
   try {
     state.search.query = query;
-    const { data } = await getJSON(`${API_URL}?search=${query}`);
+    const { data } = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
     state.search.results = data.recipes.map(rec => {
       return {
         id: rec.id,
         title: rec.title,
         publisher: rec.publisher,
-        image: rec.image_url
+        image: rec.image_url,
+        ...(rec.key && { key: rec.key })
       };
     });
     state.search.page = 1;
@@ -98,7 +99,7 @@ export async function uploadRecipe(newRecipe) {
       .filter(entry => entry[0]
         .startsWith('ingredient') && entry[1] !== '')
       .map(ing => {
-        const ingArr = ing[1].replaceAll(' ', '').split(',');
+        const ingArr = ing[1].split(',').map(el => el.trim());
         if (ingArr.length !== 3) throw new Error('Wrong ingredient format! Please use the correct format');
         const [quantity, unit, description] = ingArr;
         return { quantity: quantity ? +quantity : null, unit, description };
@@ -112,7 +113,7 @@ export async function uploadRecipe(newRecipe) {
       servings: +newRecipe.servings,
       ingredients
     };
-    const data = await sendJSON(`${API_URL}?key=${KEY}`, recipe);
+    const data = await AJAX(`${API_URL}?key=${KEY}`, recipe);
     state.recipe = createRecipeObject(data);
     addBookmark(state.recipe);
   } catch (err) {
